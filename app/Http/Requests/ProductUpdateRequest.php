5740aeeec9c39201368c76e51260ff499c6d4417
Denzel Zuñiga Noriega <zuniganoriegadenzel@gmail.com>
2025-02-22 17:01:01 -0500

    feat: Creation of ProductStoreRequest and ProductUpdateRequest

---
+++ app/Http/Requests/ProductUpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'SKU'          => 'required|string|max:20|unique:products,SKU,' . $this->route('product'),
+            'productName'  => 'required|string|max:100',
+            'unitPrice'    => 'required|numeric|min:0|max:999999.99',
+            'stock'        => 'required|integer|min:0',
+        ];
+    }
+}
